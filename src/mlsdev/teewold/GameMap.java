9cbde59640e43f3a0060865a614c69090b1e0618
Alexander Pohil <pohil@mlsdev.com>
2012-12-17 14:49:48 +0200

    Inital comit. Add base logic

---
+++ src/mlsdev/teewold/GameMap.java
@@ -0,0 +1,87 @@
+package mlsdev.teewold;
+
+import java.util.ArrayList;
+public class GameMap {
+	int[][] map;
+	
+	public GameMap(int[][] map) {
+		this.map = map;
+	}
+	
+	public void prepareMap(Point startPoint) {
+		ArrayList<Point> nextStep = new ArrayList<Point>();
+		ArrayList<Point> currStep = new ArrayList<Point>();
+		currStep.add(startPoint);
+		
+		while (currStep.size() > 0) {
+			for (Point point : currStep) {
+				int value = map[point.y][point.x];
+				if (analizeStep(point.x - 1, point.y , value)) {
+					nextStep.add(new Point(point.x - 1, point.y));
+				}
+				if (analizeStep(point.x + 1, point.y, value)) {
+					nextStep.add(new Point(point.x - 1, point.y));
+				}
+				if (analizeStep(point.x, point.y - 1, value)) {
+					nextStep.add(new Point(point.x, point.y - 1));
+				}
+				if (analizeStep(point.x, point.y + 1, value)) {
+					nextStep.add(new Point(point.x, point.y + 1));
+				}
+			}
+			currStep = nextStep;
+			nextStep = new ArrayList<Point>();
+			//printMap();
+			//System.out.println();
+		}
+	}
+	
+	public boolean analizeStep(int x, int y, int prevValue) {
+		try {
+			if (map[y][x] < 0) {
+				return false;
+			}
+			if (map[y][x] == 0 || map[y][x] > prevValue) {
+				map[y][x] = ++prevValue;
+				return true;
+			}
+			return false;
+		} catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+	
+	public Point findPoint(int value) {
+		int i = 0;
+		int j = 0;
+		while (map[i][j] != value) {
+			if (j < map[0].length - 1) {
+				j++;
+			} else {
+				j = 0;
+				i++;
+			}
+		}
+		return new Point(j, i);
+	}
+	
+	public void printMap() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				System.out.printf("%3d ",map[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	class Point {
+		int x;
+		int y;
+		
+		public Point(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
---
+++ src/mlsdev/teewold/Main.java
@@ -0,0 +1,65 @@
+package mlsdev.teewold;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public final static String INPUT_FILE_NAME = "input.txt";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world");
+		File file = new File(INPUT_FILE_NAME);
+		int width, height;
+		int[][] map = new int[0][0];
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String param[] = reader.readLine().split(" ");
+			height = Integer.parseInt(param[0]);
+			width = Integer.parseInt(param[1]);
+			map = new int[height][width];
+			for (int i = 0; i < height; i++) {
+				String line = reader.readLine();
+				for (int j = 0; j < line.length(); j++) {
+					//System.out.print(line.charAt(j));
+					switch (line.charAt(j)) {
+					case '0':
+						map[i][j] = 0;
+						break;
+					case '1':
+						map[i][j] = -1;
+						break;
+					case 'x':
+						map[i][j] = -3;
+						break;
+					case 'n':
+						map[i][j] = 1;
+						break;
+					case 'R':
+						map[i][j] = -2;
+						break;
+					default:
+						break;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		MapLogic logic = new MapLogic(map);
+		logic.printMap();
+		System.out.println();
+		logic.prepareMap();
+		//logic.printMap();
+		
+	}
+	
+	
+
+}
